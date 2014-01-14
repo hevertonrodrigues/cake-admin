271ff1f83508cf9e626f87a31dea46a93890ee42
Heverton Rodrigues <heverton25@gmail.com>
2014-01-14 00:03:04 -0200

    created auth

---
+++ app/Model/User.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * User Model
+ *
+ * @author Heverton Rodrigues
+ * @email heverton25@gmail.com
+ * @since         2014-01-13
+ * @path app/Model/User.php
+ */
+App::uses('AuthComponent', 'Controller/Component');
+class User extends AppModel {
+  public $name = 'User';
+  public $validate = array(
+    'username' => array(
+      'required' => array(
+        'rule' => array('notEmpty'),
+        'message' => 'A username is required'
+      )
+    ),
+    'password' => array(
+      'required' => array(
+        'rule' => array('notEmpty'),
+        'message' => 'A password is required'
+      )
+    ),
+    'role' => array(
+      'valid' => array(
+        'rule' => array('inList', array('admin', 'author')),
+        'message' => 'Please enter a valid role',
+        'allowEmpty' => false
+      )
+    )
+  );
+
+
+  public function beforeSave($options = array())
+  {
+    if (isset($this->data[$this->alias]['password'])) {
+      $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+    }
+    return true;
+  }
+}
